cc450c21286ed52b65e96107da476bc9a6811323
taterntots <matthew.a.weidner@gmail.com>
2020-01-06 13:37:43 -0500

    created a login page that should ping the backend server, but does not work correctly yet.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,63 @@
 import React, { useState } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 const initialFormState = {
-    username: '',
-    password: ''
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    isFetching: false
 }
 
 const Login = () => {
-    const [form, setForm] = useState(initialFormState);
+    const [credentials, setCredentials] = useState(initialFormState);
+
+    const login = (event) => {
+        event.preventDefault();
+        setCredentials({
+          isFetching: true
+        })
+        axiosWithAuth()
+          .post('/auth/login', credentials)
+          .then(res => {
+            console.log(res);
+            // localStorage.setItem('token', res.data.payload);
+            // props.history.push('/colors');
+          })
+          .catch(err => {
+            console.log(err);
+          })
+      }
+    
+      const handleChange = (event) => {
+        event.preventDefault();
+        setCredentials({
+          ...credentials,
+          [event.target.name]: event.target.value
+        })
+    }
 
     return (
         <div>
-            <hi>Welcome!</hi>
+            <h1>Welcome!</h1>
+            <form onSubmit={login}>
+                <input
+                    type='text'
+                    name='email'
+                    placeholder='email'
+                    value={credentials.email}
+                    onChange={handleChange}
+                />
+                <input
+                    type='password'
+                    name='password'
+                    placeholder='password'
+                    value={credentials.password}
+                    onChange={handleChange}
+                />
+                <button>Log in</button>
+                {credentials.isFetching && 'logging in'}
+            </form>
         </div>
     )
 }
