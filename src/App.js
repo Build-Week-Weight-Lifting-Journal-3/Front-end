46c2365000e220d8f37619925841e07aee11c246
taterntots <matthew.a.weidner@gmail.com>
2020-01-07 12:05:33 -0500

    resolved some merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Login from './components/Login';
 import RegisterAccount from './components/RegisterAccount';
 import Journal from './components/Journal';
 import Signup from "./components/Signup.js";
 import PrivateRoute from './components/PrivateRoute';
 import './App.css';
 import Login from "./components/Login";
-import { Route } from "react-router-dom";
 
 function App() {
   return (
@@ -17,7 +15,7 @@ function App() {
         <Route exact path='/' component={Login} />
         <Route path="/register" component={RegisterAccount} />
         <PrivateRoute path='/journal' component={Journal} />
-        <Route exact path='/Signup' component={Signup} />
+        {/* <Route exact path='/Signup' component={Signup} /> */}
       </main>
     </Router>
   );
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,81 +1,81 @@
-import React, { Component } from "react";
-import {
-  Button,
-  FormGroup,
-  FormControl,
-  FormLabel,
-  Form
-} from "react-bootstrap";
+// import React, { Component } from "react";
+// import {
+//   Button,
+//   FormGroup,
+//   FormControl,
+//   FormLabel,
+//   Form
+// } from "react-bootstrap";
 
-export default class Signup extends Component {
-  constructor(props) {
-    super(props);
+// export default class Signup extends Component {
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      email: "",
-      password: ""
-    };
-  }
+//     this.state = {
+//       email: "",
+//       password: ""
+//     };
+//   }
 
-  validateForm() {
-    return this.state.email.length > 0 && this.state.password.length > 6;
-  }
+//   validateForm() {
+//     return this.state.email.length > 0 && this.state.password.length > 6;
+//   }
 
-  handleChange = e => {
-    this.setState({
-      [e.target.id]: e.target.value
-    });
-  };
+//   handleChange = e => {
+//     this.setState({
+//       [e.target.id]: e.target.value
+//     });
+//   };
 
-  handleSubmit = e => {
-    e.preventDefault();
-  };
+//   handleSubmit = e => {
+//     e.preventDefault();
+//   };
 
-  render() {
-    return (
-      <div className='Signup'>
-      <Form onSubmit={this.handleSubmit}>
-      <FormGroup 
-          controlId='email' 
-          bsSize='large'>
-          <FormLabel>Email</FormLabel>
-          <FormControl
-            autoFocus
-            type='email'
-            value={this.state.email}
-            onChange={this.handleChange}
-          />
-        </FormGroup>
-        <FormGroup controlId='password' bsSize='large'>
-          <FormLabel>Password</FormLabel>
-          <FormControl
-            type='password'
-            value={this.state.password}
-            onChange={this.handleChange}
-          />
-        </FormGroup>
-        <FormGroup controlId='confirmPassword' bsSize='large'>
-          <FormLabel>Confirm Password</FormLabel>
-          <FormControl
-            type='password'
-            onChange={this.handleChange}
-            value={this.state.password}
-          />
-        </FormGroup>
+//   render() {
+//     return (
+//       <div className='Signup'>
+//       <Form onSubmit={this.handleSubmit}>
+//       <FormGroup 
+//           controlId='email' 
+//           bsSize='large'>
+//           <FormLabel>Email</FormLabel>
+//           <FormControl
+//             autoFocus
+//             type='email'
+//             value={this.state.email}
+//             onChange={this.handleChange}
+//           />
+//         </FormGroup>
+//         <FormGroup controlId='password' bsSize='large'>
+//           <FormLabel>Password</FormLabel>
+//           <FormControl
+//             type='password'
+//             value={this.state.password}
+//             onChange={this.handleChange}
+//           />
+//         </FormGroup>
+//         <FormGroup controlId='confirmPassword' bsSize='large'>
+//           <FormLabel>Confirm Password</FormLabel>
+//           <FormControl
+//             type='password'
+//             onChange={this.handleChange}
+//             value={this.state.password}
+//           />
+//         </FormGroup>
         
-        <Button
-          block
-          color='red'
-          backgroundColor='grey'
-          type='submit'
-          bsSize='large'
-          disabled={!this.validateForm()}>
-          Signup
-        </Button>
+//         <Button
+//           block
+//           color='red'
+//           backgroundColor='grey'
+//           type='submit'
+//           bsSize='large'
+//           disabled={!this.validateForm()}>
+//           Signup
+//         </Button>
           
           
-        </Form>
-      </div>
-    );
-  }
-}
+//         </Form>
+//       </div>
+//     );
+//   }
+// }
