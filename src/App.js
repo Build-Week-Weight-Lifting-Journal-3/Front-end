48db9e1b9290de51fc6abe467086b5f0bb82bf46
Gus Baden <gusbaden96@gamil.com>
2020-02-03 09:45:45 -0800

    added master react app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import './App.css';
-import RegisterAccount from './components/RegisterAccount';
-import JournalList from './components/JournalList';
-import ExerciseList from './components/ExerciseList';
-import PrivateRoute from './components/PrivateRoute';
-import Login from './components/Login';
-import AnotherJournal from './components/AnotherJournal';
-import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <Router>
-      <main className='App'>
-          <NavBar />
-          <Route exact path='/' component={Login} />
-          <Route path='/register' component={RegisterAccount} />
-        <Switch>
-          <PrivateRoute exact path='/journal' component={JournalList} />
-          <PrivateRoute exact path='/anotherjournal' component={AnotherJournal} />
-          <PrivateRoute exact path='/exercises' component={ExerciseList} />
-        </Switch>
-      </main>
-    </Router>
+    <div className="App">
+   
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,29 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { applyMiddleware, compose, createStore } from 'redux';
-import { Provider } from 'react-redux';
-import { createBrowserHistory } from 'history';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import App from './App';
-import { reducer } from './reducers';
 import './index.css';
+import App from './App';
 
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+ReactDOM.render(<App />, document.getElementById('root'));
 
-export const history = createBrowserHistory();
 
-const store = createStore(
-    reducer,
-    composeEnhancers(applyMiddleware(thunk, logger))
-)
 
-const rootElement = document.getElementById('root');
-
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    rootElement
-)
